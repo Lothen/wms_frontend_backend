1be872c7c49613167495b4459d42d13f6c821931
Gokhan <ozdemirgokhan11@gmail.com>
2025-10-24 01:30:56 +0300

    Supabase Db Connect

--- backend/database.py
+++ backend/database.py
@@ -1,3 +1,5 @@
+
+"""
 import psycopg2
 
 def get_connection():
@@ -9,5 +11,17 @@ def get_connection():
         port="5432",
         sslmode="require"
     )
+"""
 
 
+import psycopg2
+
+def get_connection():
+    return  psycopg2.connect(
+        host="db.causweuhihdqbfpgkqxi.supabase.co",
+        port=5432,
+        database="postgres",
+        user="postgres",
+        password="1453Wms1453",  # Supabase şifreni buraya yaz
+        sslmode="require"          # Supabase bağlantısı için gerekli
+    )
